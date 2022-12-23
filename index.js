77aced3c85d6543cadc3ff3e8b347e2956e13fd8
iabbakr <abubakaribrahim603@gmail.com>
2022-12-23 16:18:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+let input = ["hhhhh"];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const inputBtn2 = document.getElementById("input-btn2");
-let input = ["hhhhh"];
+
 const uList = document.getElementById("ul-el");
 
 
 
 inputBtn.addEventListener("click", function(){
     input.push(inputEl.value);
+    console.log(uList);
     
      
-} );
+});
 inputBtn2.addEventListener("click", function(){
     input.pop(inputEl.value);
-} ); 
+}); 
 
 for (let i = 0; i < input.length; i++) {
     uList.textContent+=input[i];
