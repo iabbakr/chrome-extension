a5be12ea96c7fc3008723b47fa1b18f1a7b9e167
iabbakr <abubakaribrahim603@gmail.com>
2022-12-23 15:40:47 +0100

    added changes to js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function save(){
-    console.log("button click");
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+let inputBtn2 = document.getElementById("input-btn2");
+let input = [];
+let uList = document.getElementById("ul-el");
 
-};
+
+
+inputBtn.addEventListener("click", function(){
+    input.push(inputEl.value);
+    for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i]);
+    };
+    uList.textContent=input; 
+} );
+inputBtn2.addEventListener("click", function(){
+    input.push(inputEl.value);
+} );
