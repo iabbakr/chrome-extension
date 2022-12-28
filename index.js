26ead088579aef4406114a3511b380f82f59d2b2
iabbakr <abubakaribrahim603@gmail.com>
2022-12-28 16:12:40 +0100

    added changes

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 let myInput = [];
 const inputBtn = document.getElementById("input-btn");
 const inputBtn2 = document.getElementById("input-btn2");
+const inputBtn3 = document.getElementById("input-btn3");
 const inputEl = document.getElementById("input-el");
 const uList = document.getElementById("ul-el");
 
 let inputFromLocalStorage = JSON.parse(localStorage.getItem("myInput"));
 
+// local storage
+
 if(inputFromLocalStorage){
     myInput = inputFromLocalStorage;
-    renderInput();
+    render(myInput);
+};
+const tabs = [
+    {url: "https://www.youtube.com"}
+];
+
+
+function render(input){
+    let listItems = "";
+
+    for (let i = 0; i < input.length; i++) {
+        listItems += `<li>
+        <a href='${input[i]}' target='blank'>${input[i]}
+        </a> 
+        </li>`;
+    };
+    uList.innerHTML = listItems;
 };
 
 
@@ -17,27 +36,18 @@ inputBtn.addEventListener("click", function(){
     myInput.push(inputEl.value);
     inputEl.value= "";
     localStorage.setItem("myInput", JSON.stringify(myInput))
-    renderInput();
-});
-inputBtn2.addEventListener("dblclick", function(){
-    localStorage.pop(myInput);
-    renderInput();
+    render (myInput);
 });
 
-inputBtn2.addEventListener("dblclick", function(){
+
+inputBtn2.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    
+});
+
+inputBtn3.addEventListener("dblclick", function(){
     localStorage.clear();
     myInput = [];
-    renderInput();
+    render(myInput);
 });
-function renderInput(){
-    let listItems = "";
-
-    for (let i = 0; i < myInput.length; i++) {
-        listItems += `<li>
-        <a href='${myInput[i]}' target='blank'>${myInput[i]}
-        </a> 
-        </li>`;
-    };
-    uList.innerHTML = listItems;
-};
 
